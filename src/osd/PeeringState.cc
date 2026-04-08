0a6ee90e5ea4badeff28200128d903ee688a9bef
Bill Scales <bill_scales@uk.ibm.com>
2026-04-08 17:34:45 +0100

    osd: Fix pool migration remote reservation cancelation
    Pool migration remote reservations were being canceled when
    PeeringState exited the MigratingTarget state. This caused
    cancelation messages to be sent as a new peering interval
    started.
    
    Modify the code so that all the normal state transitions out
    of this state send the cancelation messages, but that resetting
    the peering state machine does not.
    
    Signed-off-by: Bill Scales <bill_scales@uk.ibm.com>

--- src/osd/PeeringState.cc
+++ src/osd/PeeringState.cc
@@ -6317,10 +6317,19 @@ void PeeringState::MigratingTarget::migration_release_reservations()
 }
 
 boost::statechart::result
+PeeringState::MigratingTarget::react(const StopTargetPoolMigration &c)
+{
+  DECLARE_LOCALS;
+  migration_release_reservations();
+  return transit<Clean>();
+}
+
+boost::statechart::result
 PeeringState::MigratingTarget::react(const DoRecovery &c)
 {
   DECLARE_LOCALS;
   pl->on_pool_migration_target_suspended(false);
+  migration_release_reservations();
   return transit<WaitLocalRecoveryReserved>();
 }
 
@@ -6330,6 +6339,7 @@ PeeringState::MigratingTarget::react(const RemotePoolMigrationRejectedTooFull &)
   DECLARE_LOCALS;
   ps->state_set(PG_STATE_MIGRATION_TOOFULL);
   pl->on_pool_migration_target_suspended(true);
+  migration_release_reservations();
   return transit<Clean>();
 }
 
@@ -6339,6 +6349,7 @@ PeeringState::MigratingTarget::react(const RemotePoolMigrationRevokedTooFull &)
   DECLARE_LOCALS;
   ps->state_set(PG_STATE_MIGRATION_TOOFULL);
   pl->on_pool_migration_target_suspended(true);
+  migration_release_reservations();
   return transit<Clean>();
 }
 
@@ -6348,6 +6359,7 @@ PeeringState::MigratingTarget::react(const RemotePoolMigrationRevoked &)
   DECLARE_LOCALS;
   ps->state_set(PG_STATE_MIGRATION_WAIT);
   pl->on_pool_migration_target_suspended(false);
+  migration_release_reservations();
   return transit<Clean>();
 }
 
@@ -6356,7 +6368,6 @@ void PeeringState::MigratingTarget::exit()
   context< PeeringMachine >().log_exit(state_name, enter_time);
   DECLARE_LOCALS;
   ps->state_clear(PG_STATE_MIGRATING);
-  migration_release_reservations();
   utime_t dur = ceph_clock_now() - enter_time;
   pl->get_peering_perf().tinc(rs_migratingsource_latency, dur);
 }
